5881087560cfdf51ee48cc8d14f4b10c96cfc14d
ahmadakmalsyafii <barakba12344@gmail.com>
2025-09-06 09:21:58 +0700

    last

--- lib/core/errors/exceptions.dart
+++
@@ -1,4 +0,0 @@
-class ServerException implements Exception {}
-class CacheException implements Exception {}
-class GeminiException implements Exception {}
-class NoInternetException implements Exception {}
